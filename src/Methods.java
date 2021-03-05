12d716289468cf03ef9885f56e835a48ef10fbad
Isaac Busee <58541303+isakabusee@users.noreply.github.com>
2021-03-05 00:59:42 -0800

    first commit

---
+++ src/Methods.java
@@ -0,0 +1,18 @@
+
+public class Methods {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 10;
+		System.out.println(a * b);
+
+	}
+	public static void add() {
+		int num1 = 1;
+		int num2 = 4;
+		 
+		System.out.println(num1 + num2);
+		
+	}
+
+}
